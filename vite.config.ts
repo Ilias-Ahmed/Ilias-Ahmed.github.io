f22c070b0180a029fae2fdefd1d21f5687d1d4eb
Ilias Ahmed <iliasahmed70023@gmail.com>
2025-05-26 14:04:39 +0530

    feat: add vite-plugin-gh-pages and update package.json scripts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio/", // Change "portfolio" to your actual GitHub repo name
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,3 +13,5 @@ export default defineConfig({
     },
   },
 });
+
+
